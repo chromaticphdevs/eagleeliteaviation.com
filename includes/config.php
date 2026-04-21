3fd161b7872f4e69fb19a77867d9d5d2a8c3d674
Mark Angelo Gonzales <chromaticsoftwares@gmail.com>
2026-04-22 01:14:52 +0800

    add all cahnges

--- includes/config.php
+++ includes/config.php
@@ -1,2 +1,9 @@
 <?php
-define('BASE', 'https://eagleeliteaviation.com');
+// Set to 'dev' when working locally, 'prod' for live site
+define('ENV', 'dev');
+
+if (ENV === 'dev') {
+    define('BASE', '/eagleeliteaviation.com');
+} else {
+    define('BASE', 'https://eagleeliteaviation.com');
+}
